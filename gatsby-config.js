3a49f5dd55d13271a70522f75898f52833848c0e
Marco Martino <mecmartini@gmail.com>
2019-03-01 16:38:51 +0100

    Set gatsby title and author meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Drupal Gatsby Test`,
+    description: `Drupal Gatsby Test by Soulweb.`,
+    author: `@soulwebsolutions`,
     articlesLink: `/articles`
   },
   plugins: [
